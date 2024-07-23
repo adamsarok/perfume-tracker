996c1c8597559bcf0c17dec777fddc8828befc8b
Ádám Sarok <sarok.adam@gmail.com>
2024-07-23 23:39:31 +0200

    this should have been a combobox

--- src/components/perfume-worn-table.tsx
+++ src/components/perfume-worn-table.tsx
@@ -2,28 +2,27 @@
 
 import { PerfumeWornDTO } from "@/app/actions";
 import {  Checkbox, Divider, Link, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, getKeyValue } from "@nextui-org/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {useAsyncList} from "@react-stately/data";
 import React from "react";
+import { set } from "zod";
 
 export interface PerfumeWornTableProps {
     perfumes: PerfumeWornDTO[]
 }
-export default function PerfumeWornTable({ perfumes }: PerfumeWornTableProps) {
-    const [isSelected, setIsSelected] = React.useState(true);
-    const handleCheckboxChange = (newValue: boolean) => {
-      setIsSelected(newValue);
-      
-      //TODO: how to I reload the table?
-      //perfumes = await actions.GetAllPerfumesWithWearCount(isSelected);
-    }
 
+function Reload({ perfumes }: PerfumeWornTableProps) {
     
+}
+
+export default function PerfumeWornTable({ perfumes }: PerfumeWornTableProps) {
     let list = useAsyncList({
         async load({signal}) {
+            //console.log('isSelected in load:', isSelected);
             return {
-                items: (isSelected ? 
-                    perfumes.filter((x) => x.perfume.rating >=8 && x.perfume.ml > 0) : 
+                items: (isGoodStuff ? 
+                    perfumes.filter((x) => x.perfume.rating >=8 && x.perfume.ml > 0) :
+                    isBuyStuff ? perfumes.filter((x) => x.perfume.rating >=8 && x.perfume.ml <= 0) :
                     perfumes)
             };
         },
@@ -50,10 +49,25 @@ export default function PerfumeWornTable({ perfumes }: PerfumeWornTableProps) {
         },
     });
 
+    //should be combobox TODO
+
+    const [isGoodStuff, setIsGoodStuff] = React.useState(true);
+    const [isBuyStuff, setBuyStuff] = React.useState(false);
+
+    useEffect(() => {
+        list.reload();
+    }, [isGoodStuff]); // Reload the list whenever isSelected changes
+    useEffect(() => {
+        list.reload();
+    }, [isBuyStuff]); // Reload the list whenever isSelected changes
+
 
     return <div>
-        <Checkbox isSelected={isSelected} onValueChange={setIsSelected}>
+        <Checkbox className="ml-2 mr-6" isSelected={isGoodStuff} onValueChange={setIsGoodStuff}>
             Show only good stuff
+        </Checkbox>
+        <Checkbox isSelected={isBuyStuff} onValueChange={setBuyStuff}>
+            Buy list
         </Checkbox>
         <Divider></Divider>
         <Table isStriped
