62161b16678812686b351d2592f48e8a4d570a40
Ádám Sarok <sarok.adam@gmail.com>
2025-01-20 22:14:25 +0100

    feat: seasonal suggestions

--- PerfumeTracker.Server/Repo/PerfumeSuggestedRepo.cs
+++ PerfumeTracker.Server/Repo/PerfumeSuggestedRepo.cs
@@ -32,13 +32,27 @@ public class PerfumeSuggestedRepo(PerfumetrackerContext context, PerfumeWornRepo
             .ToListAsync();
         return r;
     }
+	enum Seasons { Winter, Spring, Summer, Autumn };
+	private Seasons Season => DateTime.Now.Month switch {
+		1 or 2 or 12 => Seasons.Winter,
+		3 or 4 or 5 => Seasons.Spring,
+		6 or 7 or 8 => Seasons.Summer,
+		9 or 10 or 11 => Seasons.Autumn,
+	};
     public async Task<int> GetPerfumeSuggestion(int daysFilter) {
         var alreadySug = await GetAlreadySuggestedPerfumeIds(daysFilter);
         var worn = await perfumeWornRepo.GetWornPerfumeIDs(daysFilter);
-        var result = await context
+		var season = Season;
+		var result = await context
             .Perfumes
             .Where(x => !alreadySug.Contains(x.Id) && !worn.Contains(x.Id))
             .Where(x => x.Ml > 0 && x.Rating >= 8)
+			.Where(x => (!x.Winter && !x.Spring && !x.Summer && !x.Autumn) || 
+				(season == Seasons.Autumn && x.Autumn) ||
+				(season == Seasons.Winter && x.Winter) ||
+				(season == Seasons.Spring && x.Spring) ||
+				(season == Seasons.Summer && x.Summer) 
+			)
             .Select(x => x.Id)
             .FirstOrDefaultAsync();
         await AddSuggestedPerfume(result);
