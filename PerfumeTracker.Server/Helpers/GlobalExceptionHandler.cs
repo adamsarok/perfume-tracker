a49f1fe694d624cb01d7e573bb27bfdf1a78eef2
Ádám Sarok <sarok.adam@gmail.com>
2025-03-03 21:48:45 +0100

    feat: better error handling

--- PerfumeTracker.Server/Helpers/GlobalExceptionHandler.cs
+++ PerfumeTracker.Server/Helpers/GlobalExceptionHandler.cs
@@ -9,20 +9,42 @@ namespace PerfumeTrackerAPI.Server.Helpers {
             Exception exception,
             CancellationToken cancellationToken) {
             logger.LogError(
-                exception, "Exception occurred: {Message}", exception.Message);
+                exception, "Exception occurred: {Message} at {Time}", exception.Message, DateTime.UtcNow);
 
-            var problemDetails = new ProblemDetails {
-                Status = exception is NotFoundException ? StatusCodes.Status404NotFound : StatusCodes.Status500InternalServerError,
-                Title = "Server error",
-                Detail = exception.Message
-            };
-
-            httpContext.Response.StatusCode = problemDetails.Status.Value;
-
-            await httpContext.Response
-                .WriteAsJsonAsync(problemDetails, cancellationToken);
-
-            return true;
-        }
+			(string Detail, string Title, int StatusCode) details = exception switch {
+				//ValidationException => (
+				//	exception.Message,
+				//	exception.GetType().Name,
+				//	context.Response.StatusCode = StatusCodes.Status400BadRequest
+				//),
+				MappingException => (
+					exception.Message,
+					exception.GetType().Name,
+					httpContext.Response.StatusCode = StatusCodes.Status400BadRequest
+				),
+				NotFoundException => (
+					exception.Message,
+					exception.GetType().Name,
+					httpContext.Response.StatusCode = StatusCodes.Status404NotFound
+				),
+				_ => (
+					exception.Message,
+					exception.GetType().Name,
+					httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError
+				)
+			};
+			var problemDetails = new ProblemDetails {
+				Title = details.Title,
+				Detail = details.Detail,
+				Status = details.StatusCode,
+				Instance = httpContext.Request.Path
+			};
+			problemDetails.Extensions.Add("traceId", httpContext.TraceIdentifier);
+			//if (exception is ValidationException validationException) { //TODO when fluentValidation is implemented
+			//	problemDetails.Extensions.Add("ValidationErrors", validationException.Errors);
+			//}
+			await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+			return true;
+		}
     }
 }
