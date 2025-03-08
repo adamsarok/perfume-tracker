a51db01de358ed7fd690b1e7087b06029d8e79c2
Ádám Sarok <sarok.adam@gmail.com>
2025-03-08 07:38:51 +0100

    feat: health check

--- PerfumeTracker.Server/Program.cs
+++ PerfumeTracker.Server/Program.cs
@@ -1,4 +1,5 @@
 using Carter;
+using HealthChecks.UI.Client;
 using Microsoft.EntityFrameworkCore;
 using PerfumeTracker.Server;
 using PerfumeTracker.Server.Repo;
@@ -11,8 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 string? conn;
 conn = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 if (string.IsNullOrWhiteSpace(conn)) conn = builder.Configuration.GetConnectionString("DefaultConnection");
-if (string.IsNullOrWhiteSpace(conn)) throw new Exception("Connection string is empty");
-
 if (string.IsNullOrWhiteSpace(conn)) throw new ConfigEmptyException("Connection string is empty");
 
 builder.Services.AddDbContext<PerfumetrackerContext>(opt => opt.UseNpgsql(conn));
@@ -22,6 +21,9 @@ builder.Services.AddScoped<PerfumeWornRepo>();
 builder.Services.AddScoped<PerfumeSuggestedRepo>();
 builder.Services.AddScoped<RecommendationsRepo>();
 builder.Services.AddCarter();
+
+builder.Services.AddHealthChecks()
+	.AddNpgSql(conn);
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
@@ -42,6 +44,9 @@ app.UseCors(x => x.AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
     .WithOrigins("http://localhost", "http://192.168.1.79:3000", "https://192.168.1.79:3000", "https://localhost:3000", "http://localhost:3000"));
+app.UseHealthChecks("/api/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions {
+	ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
 
