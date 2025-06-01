128771bc29cd3ab341657b4f90e93fd2a37dfd11
Ádám Sarok <sarok.adam@gmail.com>
2025-06-02 00:10:33 +0200

    fix: docker compose

--- perfumetracker.client/src/app/logs/page.tsx
+++ perfumetracker.client/src/app/logs/page.tsx
@@ -1,14 +1,15 @@
 import LogsComponent from "./log-component";
 
+export const dynamic = 'force-dynamic'
+
 export default function LogsPage() {
   const apiUrl = process.env.NEXT_PUBLIC_PERFUMETRACKER_API_ADDRESS;
   if (!apiUrl) {
     console.error("API URL not configured");
-    return;
+    return <div>API URL not configured</div>;
   }
 
   return <div>
-    <h1>Logs</h1>
     <LogsComponent apiUrl={apiUrl}></LogsComponent>
   </div>
 } 
