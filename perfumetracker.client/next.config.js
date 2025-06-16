40634d75f36e9cdfed4412e3c8bb58407e13c542
Ádám Sarok <sarok.adam@gmail.com>
2025-06-16 21:37:36 +0200

    fix:csp

--- perfumetracker.client/next.config.js
+++ perfumetracker.client/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'Content-Security-Policy-Report-Only',
+            key: 'Content-Security-Policy',
             value: `
               default-src 'none';
               base-uri 'self';
