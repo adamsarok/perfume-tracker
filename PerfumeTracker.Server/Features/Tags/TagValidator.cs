e405bae52487c4dd10f48172efe72f1390e167ab
Ádám Sarok <sarok.adam@gmail.com>
2025-08-03 20:09:38 +0200

    fix: misc

--- PerfumeTracker.Server/Features/Tags/TagValidator.cs
+++ PerfumeTracker.Server/Features/Tags/TagValidator.cs
@@ -1,6 +1,7 @@
-﻿public class TagValidator : AbstractValidator<TagUploadDto> {
+﻿namespace PerfumeTracker.Server.Features.Tags;
+public class TagValidator : AbstractValidator<TagUploadDto> {
 	public TagValidator() {
 		RuleFor(x => x.TagName).Length(1, 250);
-		RuleFor(x => x.Color).Length(7).Matches("#([A-F0-9])+").WithErrorCode("Not a valid hex color code");
+		RuleFor(x => x.Color).Length(7).Matches("^#[A-F0-9]{6}$").WithMessage("Not a valid hex color code");
 	}
 }
