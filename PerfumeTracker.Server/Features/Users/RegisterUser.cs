d81ad544d74dcc0a59cd2c33d128d4e1a2e222d9
Ádám Sarok <sarok.adam@gmail.com>
2025-08-31 11:12:16 +0200

    fix: invite burn

--- PerfumeTracker.Server/Features/Users/RegisterUser.cs
+++ PerfumeTracker.Server/Features/Users/RegisterUser.cs
@@ -26,11 +26,23 @@ public class RegisterUserEndpoint : ICarterModule {
 public class RegisterUserHandler(ICreateUser createUser, IConfiguration configuration, PerfumeTrackerContext context) : ICommandHandler<RegisterUserCommand, Unit>  {
 	public async Task<Unit> Handle(RegisterUserCommand command, CancellationToken cancellationToken) {
 		var userConfig = new UserConfiguration(configuration);
+		Invite? invite = null;
 		if (userConfig.InviteOnlyRegistration) {
-			var affected = await context.Invites
-				.Where(x => x.Id == command.InviteCode && x.Email == command.Email && !x.IsUsed)
-				.ExecuteUpdateAsync(s => s.SetProperty(i => i.IsUsed, true), cancellationToken);
-			if (affected < 1) throw new UnauthorizedException("Invite not valid or already used");
+			using var transaction = await context.Database.BeginTransactionAsync();
+			try {
+				invite = await context.Invites
+					.Where(x => x.Email == command.Email && x.Id == command.InviteCode)
+					.FirstOrDefaultAsync(cancellationToken);
+				if (invite == null) throw new UnauthorizedException("Active invite code not found for this email address");
+				await createUser.Create(command.UserName, command.Password, Roles.USER, command.Email);
+				invite.IsUsed = true;
+				await context.SaveChangesAsync(cancellationToken);
+				await transaction.CommitAsync(cancellationToken);
+				return Unit.Value;
+			} catch {
+				await transaction.RollbackAsync(cancellationToken);
+				throw;
+			}
 		}
 		await createUser.Create(command.UserName, command.Password, Roles.USER, command.Email);
 		return Unit.Value;
