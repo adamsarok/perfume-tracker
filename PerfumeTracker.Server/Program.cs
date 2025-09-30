1e2b9ebc8230f6432295815ca240978e1a9be033
Ádám Sarok <sarok.adam@gmail.com>
2025-09-30 20:16:26 +0200

    feat: mediatR license

--- PerfumeTracker.Server/Program.cs
+++ PerfumeTracker.Server/Program.cs
@@ -72,6 +72,8 @@ Startup.SetupAuthorizations(builder.Services, builder.Configuration);
 
 var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config => {
+	var licenseKey = builder.Configuration.GetValue<string>("Mediatr:LicenseKey");
+	if (!string.IsNullOrWhiteSpace(licenseKey)) config.LicenseKey = licenseKey;
 	config.RegisterServicesFromAssembly(assembly);
 	config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
