be8e5fd1eb0e638c786cddd3eed27f9421c81189
Ádám Sarok <sarok.adam@gmail.com>
2025-12-15 23:31:30 +0100

    fail

---
+++ PerfumeTracker.Server/DbContext/PerfumeTrackerContextFactory.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore.Design;
+using PerfumeTracker.Server.Services.Auth;
+
+namespace PerfumeTracker.Server.DbContext;
+
+public class PerfumeTrackerContextFactory : IDesignTimeDbContextFactory<PerfumeTrackerContext> {
+	public PerfumeTrackerContext CreateDbContext(string[] args) {
+		var optionsBuilder = new DbContextOptionsBuilder<PerfumeTrackerContext>();
+		
+		// This connection string is only used for design-time operations (migrations, etc.)
+		// It will be overridden at runtime by Program.cs
+		var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__PerfumeTracker") 
+			?? "Host=localhost;Database=PerfumeTracker;Username=postgres;Password=postgres";
+		
+		optionsBuilder.UseNpgsql(connectionString);
+		
+		// Use a null tenant provider for design-time operations
+		return new PerfumeTrackerContext(optionsBuilder.Options, new DesignTimeTenantProvider());
+	}
+}
+
+// Design-time tenant provider that returns null (no tenant filtering during migrations)
+public class DesignTimeTenantProvider : ITenantProvider {
+	public Guid? GetCurrentUserId() => null;
+}
