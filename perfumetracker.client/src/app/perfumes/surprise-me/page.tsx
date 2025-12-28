5c6539cfeca811cd4d88a7f2b022401c8de726e9
Ádám Sarok <sarok.adam@gmail.com>
2025-12-28 19:40:33 +0100

    refactor: remove old recommenders

--- perfumetracker.client/src/app/perfumes/surprise-me/page.tsx
+++
@@ -1,9 +0,0 @@
-import SuprisePerfumeComponent from "./surprise-me-component";
-
-export const dynamic = "force-dynamic";
-
-export default function SuprisePerfumePage() {
-  return (
-    <SuprisePerfumeComponent />
-  );
-}
