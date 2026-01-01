2464695ac380d1618a5c2e8a4cfd5cd7bd33f3cd
Ádám Sarok <sarok.adam@gmail.com>
2026-01-01 11:48:12 +0100

    fix: logic

--- PerfumeTracker.Server/Features/PerfumeRatings/Services/RatingService.cs
+++ PerfumeTracker.Server/Features/PerfumeRatings/Services/RatingService.cs
@@ -10,6 +10,7 @@ public class RatingService(PerfumeTrackerContext context) : IRatingService {
 			RatingDate = DateTime.UtcNow,
 		};
 		context.PerfumeRatings.Add(evt);
+		await context.SaveChangesAsync(cancellationToken);
 		await UpdatePerfume(PerfumeId, cancellationToken);
 		await context.SaveChangesAsync(cancellationToken);
 		return evt;
@@ -19,15 +20,16 @@ public class RatingService(PerfumeTrackerContext context) : IRatingService {
 		if (context.TenantProvider?.GetCurrentUserId() == null) throw new TenantNotSetException();
 		var rating = await context.PerfumeRatings.FindAsync([RatingId], cancellationToken) ?? throw new NotFoundException("PerfumeRatings", RatingId);
 		rating.IsDeleted = true;
+		await context.SaveChangesAsync(cancellationToken);
 		await UpdatePerfume(rating.PerfumeId, cancellationToken);
-		await context.SaveChangesAsync();
+		await context.SaveChangesAsync(cancellationToken);
 		return rating;
 	}
 
 	private async Task UpdatePerfume(Guid perfumeId, CancellationToken cancellationToken) {
 		var perfume = await context.Perfumes.FindAsync([perfumeId], cancellationToken) ?? throw new NotFoundException("Perfumes", perfumeId);
 		perfume.AverageRating = await context.PerfumeRatings
-			.Where(r => r.PerfumeId == perfumeId && !r.IsDeleted)
+			.Where(r => r.PerfumeId == perfumeId)
 			.AverageAsync(r => (decimal?)r.Rating, cancellationToken) ?? 0;
 	}
 }
