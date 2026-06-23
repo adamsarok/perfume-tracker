cc46af2c9c8f47ccae9c5bbf8ced207d5b93bfa4
Ádám Sarok <sarok.adam@gmail.com>
2026-06-23 08:14:36 +0200

    backfill

---
+++ PerfumeTracker.Server/Migrations/20260623060611_LatestRating.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PerfumeTracker.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class LatestRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AverageRating",
+                table: "Perfume",
+                newName: "LatestRating");
+
+            migrationBuilder.Sql(@"
+                UPDATE ""Perfume"" p
+                SET ""LatestRating"" = COALESCE((SELECT pr.""Rating""
+                    FROM ""PerfumeRating"" pr
+                    WHERE pr.""PerfumeId"" = p.""Id""
+                    AND pr.""IsDeleted"" = FALSE
+                    ORDER BY pr.""RatingDate"" DESC, pr.""CreatedAt"" DESC
+                    LIMIT 1), 0)
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LatestRating",
+                table: "Perfume",
+                newName: "AverageRating");
+        }
+    }
+}
